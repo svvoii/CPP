46cc4afa2f3c97f8c7d8c680302cffeeb30a4962
Serge <serge@Serges-MacBook-Pro.local>
2023-10-17 23:12:14 +0200

    cpp_0 ex00

--- cpp_42/cpp_00/ex00/megaphone.cpp
+++ cpp_42/cpp_00/ex00/megaphone.cpp
@@ -2,5 +2,16 @@
 #include <iostream>
 
 int	main(int argc, char **argv) {
-	std::cout << "MEGAPHONE ACTIVATED: " << std::endl;
+
+	if (argc > 1) {
+		for (int i = 1; i < argc; i++) {
+
+			for (int j = 0; argv[i][j]; j++) {
+				std::cout << (char)std::toupper(argv[i][j]);
+			}
+		}
+		std::cout << std::endl;
+	}
+	else
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
 }
